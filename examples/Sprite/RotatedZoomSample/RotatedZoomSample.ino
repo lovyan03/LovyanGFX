cb617641cdb1fe7364a868ce14fb68411e0b5e77
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-04-22 14:30:02 +0900

    bugfix: RotatedZoomSample

--- examples/Sprite/RotatedZoomSample/RotatedZoomSample.ino
+++ examples/Sprite/RotatedZoomSample/RotatedZoomSample.ino
@@ -8,7 +8,7 @@ static LGFX lcd;
 static LGFX_Sprite sprite1(&lcd);
 static LGFX_Sprite sprite2(&lcd);
 static LGFX_Sprite* sprites[2] = {&sprite1, &sprite2};
-static int32_t width;
+static int32_t width = 280;
 static size_t count = 0;
 
 void setup(void)
@@ -20,7 +20,7 @@ void setup(void)
 
   lcd.init();
   lcd.setPivot(lcd.width()>>1, lcd.height()>>1);
-  width = (std::max(lcd.width(), lcd.height())+10) | 1;
+  width = std::min(width, (std::max(lcd.width(), lcd.height())+10)) | 1;
   for (int i = 0; i < 2; ++i) {
     sprites[i]->setColorDepth(8);
     sprites[i]->createSprite(width, width);
