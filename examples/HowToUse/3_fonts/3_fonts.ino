28e101392685b450f96d945e760cb18bbef4d586
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-05-21 22:56:48 +0900

    add examples/Sprite/PartyParrot refactoring

--- examples/HowToUse/3_fonts/3_fonts.ino
+++ examples/HowToUse/3_fonts/3_fonts.ino
@@ -20,7 +20,7 @@ void setup(void)
 
   drawGradation();
 
-  // 文字の表示方法には大きく分けて２通り、print 系の関数と drawString 系の関数があります。
+  // 文字の描画方法には大きく分けて２通り、print 系の関数と drawString 系の関数があります。
 
   // drawString 関数では、第１引数で文字列を指定し、第２引数でX座標、第３引数でY座標を指定します。
   lcd.drawString("string!", 10, 10);
@@ -31,20 +31,20 @@ void setup(void)
   // drawFloat 関数では、第１引数が数値、第２引数が小数点以下の桁数になり、第３引数がX座標、第４引数がY座標になります。
   lcd.drawFloat(3.14, 2, 150, 10);
 
-  // print 関数では、setCursor関数で指定した座標 (またはprint関数で最後に表示した文字の続き)に表示します。
+  // print 関数では、setCursor関数で指定した座標 (またはprint関数で最後に描画した文字の続き)に描画します。
   lcd.setCursor(10, 20);
   lcd.print("print!");
 
-  // printf関数で、第２引数以降の内容を表示できます。(C言語のprintf準拠ですので文字列や浮動小数も表示できます)
+  // printf関数で、第２引数以降の内容を描画できます。(C言語のprintf準拠ですので文字列や浮動小数も描画できます)
   int value = 123;
   lcd.printf("test %d", value);
 
-  // println関数で、文字列を表示後に改行できます。print("\n");と同じ効果です。
+  // println関数で、文字列を描画後に改行できます。print("\n");と同じ効果です。
   lcd.println("println");
 
   // フォントを変更するには、setFont関数を使用します。
   // TFT_eSPIのsetTextFont関数と同じフォントは Font0 ～ Font8 になります。
-  // ※ エディタの入力支援が使える場合、引数にfonts::まで入力する事でフォント一覧が表示されます
+  // ※ エディタの入力支援が使える場合、引数に&fonts::まで入力する事でフォント一覧が表示されます
   lcd.setFont(&fonts::Font4);
   lcd.println("TestFont4");
 
@@ -69,15 +69,15 @@ void setup(void)
   lcd.print("YellowText ClearBack");
 
 
-  // ※ Font6は時計表示用の文字のみが収録されています。
+  // Font6は時計用の文字のみが収録されています。
   lcd.setFont(&fonts::Font6);
   lcd.print("apm.:-0369");
 
-  // ※ Font7は７セグメント液晶風のフォントが収録されています。
+  // Font7は７セグメント液晶風のフォントが収録されています。
   lcd.setFont(&fonts::Font7);
   lcd.print(".:-147");
 
-  // ※ Font8は数字のみが収録されています。
+  // Font8は数字のみが収録されています。
   lcd.setFont(&fonts::Font8);
   lcd.print(".:-258");
 
@@ -126,7 +126,7 @@ void setup(void)
   lcd.drawString("top_left",  lcd.width() / 2,  lcd.height() / 2);
 
 
-  // 基準座標に中心線を表示
+  // 基準座標に中心線を描画
   lcd.drawFastVLine(lcd.width() / 2, 0, lcd.height(), 0xFFFFFFU);
   lcd.drawFastHLine(0, lcd.height() / 2, lcd.width(), 0xFFFFFFU);
 
@@ -145,7 +145,7 @@ void setup(void)
   lcd.setClipRect(10, 10, lcd.width() - 20, lcd.height() - 20);
 
 
-  // setTextSize 関数で 文字の表示倍率を指定します。
+  // setTextSize 関数で 文字の拡大率を指定します。
   // 第１引数で横方向の倍率、第２引数で縦方向の倍率を指定します。
   // 第２引数を省略した場合は、第１引数の倍率が縦と横の両方に反映されます。
   lcd.setTextSize(3, 4);
@@ -159,19 +159,22 @@ void setup(void)
 
   lcd.setTextSize(1);
 
-  // setTextWrap 関数で、print 関数が画面端に到達した時の折り返し動作を指定します。
-  // 第１引数が行末での折り返し、第２引数で画面下端で上端への折り返しを指定します。
-  // 第２引数は省略時 false として扱われます。
+  // setTextWrap 関数で、print 関数が画面端(描画範囲端)に到達した時の折り返し動作を指定します。
+  // 第１引数をtrueにすると、右端到達後に左端へ移動します。
+  // 第２引数をtrueにすると、下端到達後に上端へ移動します。(省略時:false)
   lcd.setTextWrap(false);
   lcd.setTextColor(0x00FFFFU, 0);
   lcd.println("setTextWrap(false) testing... long long long long string wrap test string ");
+  // false指定時は位置調整されず、描画範囲外にはみ出した部分は描画されません。
 
   lcd.setTextWrap(true);
   lcd.setTextColor(0xFFFF00U, 0);
   lcd.println("setTextWrap(true) testing... long long long long string wrap test string ");
+  // true指定時は描画範囲内に収まるよう座標を自動調整します。
 
   delay(1000);
 
+  // 第２引数にtrue指定時は、画面下端に到達すると続きを上端から描画します。
   lcd.setTextColor(0xFFFFFFU, 0);
   lcd.setTextWrap(true, true);
   lcd.println("setTextWrap(true, true) testing...");
@@ -184,7 +187,7 @@ void setup(void)
   drawGradation();
 
   // setTextScroll 関数で、画面下端に到達した時のスクロール動作を指定します。
-  // setScrollRect 関数でスクロールする矩形範囲を指定できます。
+  // setScrollRect 関数でスクロールする矩形範囲を指定します。(未指定時は画面全体がスクロールします)
   // ※ スクロール機能は、LCDが画素読出しに対応している必要があります。
   lcd.setTextScroll(true);
 
@@ -196,10 +199,10 @@ void setup(void)
   }
 
 
-  // setClipRectの範囲指定を解除
+  // setClipRectの範囲指定を解除します。
   lcd.clearClipRect();
 
-  // setScrollRectの範囲指定を解除
+  // setScrollRectの範囲指定を解除します。
   lcd.clearScrollRect();
 
 
