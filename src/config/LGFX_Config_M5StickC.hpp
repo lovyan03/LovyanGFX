7ee47645c8c75530aa3991451b15ba44d67e8c0d
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-07-20 21:43:47 +0900

    add: support M5StickCPlus ( untested )

--- src/config/LGFX_Config_M5StickC.hpp
+++ src/config/LGFX_Config_M5StickC.hpp
@@ -22,6 +22,41 @@ public:
 
     setPanel(&panel);
   }
+
+  void initPanel(void) override
+  {
+    if (!_panel) return;
+    _panel->init();
+    lgfx::LGFX_SPI<lgfx::LGFX_Config>::initPanel();
+
+    std::uint32_t id = readPanelID();
+    if (id != 0xf0897c) {  // check panel (ST7735 or ST7789)
+      ESP_LOGI("LovyanGFX", "[Autodetect] Using Panel_ST7789");
+
+      static lgfx::Panel_ST7789 panel;
+      panel.spi_3wire  = true;
+      panel.invert     = true;
+      panel.spi_cs   =  5;
+      panel.spi_dc   = 23;
+      panel.gpio_rst = 18;
+      panel.freq_write = 80000000;
+      panel.freq_read  = 16000000;
+      panel.freq_fill  = 80000000;
+      panel.spi_mode_read = 1;
+      panel.len_dummy_read_pixel = 16;
+      panel.panel_width  = 135;
+      panel.panel_height = 240;
+      panel.offset_x = 52;
+      panel.offset_y = 40;
+      panel.offset_rotation = 2;
+
+      setPanel(&panel);
+      lgfx::LGFX_SPI<lgfx::LGFX_Config>::initPanel();
+    } else {
+      ESP_LOGI("LovyanGFX", "[Autodetect] Using Panel_ST7735");
+    }
+  }
+
 };
 
 #endif
