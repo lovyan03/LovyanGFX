5aa4ea15e095b05d41bf107c5ec327af6193a49f
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2020-11-30 21:06:13 +0900

    add efont.

---
+++ src/Fonts/efont/lgfx_efont_ja.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#define U8G2_USE_LARGE_FONTS
+
+extern const uint8_t lgfx_efont_ja_10[];
+extern const uint8_t lgfx_efont_ja_10_b[];
+extern const uint8_t lgfx_efont_ja_10_bi[];
+extern const uint8_t lgfx_efont_ja_10_i[];
+extern const uint8_t lgfx_efont_ja_12[];
+extern const uint8_t lgfx_efont_ja_12_b[];
+extern const uint8_t lgfx_efont_ja_12_bi[];
+extern const uint8_t lgfx_efont_ja_12_i[];
+extern const uint8_t lgfx_efont_ja_14[];
+extern const uint8_t lgfx_efont_ja_14_b[];
+extern const uint8_t lgfx_efont_ja_14_bi[];
+extern const uint8_t lgfx_efont_ja_14_i[];
+extern const uint8_t lgfx_efont_ja_16[];
+extern const uint8_t lgfx_efont_ja_16_b[];
+extern const uint8_t lgfx_efont_ja_16_bi[];
+extern const uint8_t lgfx_efont_ja_16_i[];
+extern const uint8_t lgfx_efont_ja_24[];
+extern const uint8_t lgfx_efont_ja_24_b[];
+extern const uint8_t lgfx_efont_ja_24_bi[];
+extern const uint8_t lgfx_efont_ja_24_i[];
---
+++ src/Fonts/efont/lgfx_efont_kr.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#define U8G2_USE_LARGE_FONTS
+
+extern const uint8_t lgfx_efont_kr_10[];
+extern const uint8_t lgfx_efont_kr_10_b[];
+extern const uint8_t lgfx_efont_kr_10_bi[];
+extern const uint8_t lgfx_efont_kr_10_i[];
+extern const uint8_t lgfx_efont_kr_12[];
+extern const uint8_t lgfx_efont_kr_12_b[];
+extern const uint8_t lgfx_efont_kr_12_bi[];
+extern const uint8_t lgfx_efont_kr_12_i[];
+extern const uint8_t lgfx_efont_kr_14[];
+extern const uint8_t lgfx_efont_kr_14_b[];
+extern const uint8_t lgfx_efont_kr_14_bi[];
+extern const uint8_t lgfx_efont_kr_14_i[];
+extern const uint8_t lgfx_efont_kr_16[];
+extern const uint8_t lgfx_efont_kr_16_b[];
+extern const uint8_t lgfx_efont_kr_16_bi[];
+extern const uint8_t lgfx_efont_kr_16_i[];
+extern const uint8_t lgfx_efont_kr_24[];
+extern const uint8_t lgfx_efont_kr_24_b[];
+extern const uint8_t lgfx_efont_kr_24_bi[];
+extern const uint8_t lgfx_efont_kr_24_i[];
