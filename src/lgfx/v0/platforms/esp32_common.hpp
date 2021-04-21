597c81515d1f9a4d5ff9d6d892373fce4ffea77a
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2021-04-21 16:12:06 +0900

    tweak delay function for ESP32.

--- src/lgfx/v0/platforms/esp32_common.hpp
+++ src/lgfx/v0/platforms/esp32_common.hpp
@@ -15,11 +15,23 @@
   #include <freertos/task.h>
   #include <driver/gpio.h>
 
-  __attribute__ ((unused)) static inline void delay(std::uint32_t ms) { vTaskDelay(ms / portTICK_PERIOD_MS); }
-
   __attribute__ ((unused)) static inline unsigned long millis(void) { return (unsigned long) (esp_timer_get_time() / 1000ULL); }
 
   __attribute__ ((unused)) static inline unsigned long micros(void) { return (unsigned long) (esp_timer_get_time()); }
+
+  __attribute__ ((unused)) static inline void delayMicroseconds(std::uint32_t us) { ets_delay_us(us); }
+
+  __attribute__ ((unused)) static inline void delay(std::uint32_t ms)
+  {
+    std::uint32_t time = micros();
+    vTaskDelay( (ms >= portTICK_PERIOD_MS) ? (ms / portTICK_PERIOD_MS - 1) : 0);
+    ms *= 1000;
+    time = micros() - time;
+    if (time < ms)
+    {
+      ets_delay_us(ms - time);
+    }
+  }
 
 #endif
 
