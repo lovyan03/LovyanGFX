c49c6b008a58e212ac1aac3de7631bd0e623a196
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2021-06-05 16:25:53 +0900

    tweak XPT2046.

--- src/lgfx/v1/touch/Touch_XPT2046.cpp
+++ src/lgfx/v1/touch/Touch_XPT2046.cpp
@@ -48,61 +48,70 @@ namespace lgfx
     return true;
   }
 
-  std::uint_fast8_t Touch_XPT2046::getTouchRaw(touch_point_t *tp, std::uint_fast8_t number)
+  std::uint_fast8_t Touch_XPT2046::getTouchRaw(touch_point_t* __restrict__ tp, std::uint_fast8_t number)
   {
     tp->size = 0;
 
     if (!_inited || number != 0) return 0;
 
-    if (_cfg.pin_int >= 0)
+    if (_cfg.pin_int >= 0 && lgfx::gpio_in(_cfg.pin_int))
     {
-      if (lgfx::gpio_in(_cfg.pin_int)) return 0;
+      return 0;
     }
 
-    std::uint_fast16_t xt[21], yt[21], zt[21];
     std::uint8_t data[57];
-    for (std::size_t i = 0; i < 3; ++i)
+
+    memset(data, 0, 8);
+    data[ 0] = 0x91;
+    data[ 2] = 0xB1;
+    data[ 4] = 0xD1;
+    data[ 6] = 0xC1;
+    data[56] = 0x80; // last power off.
+    memcpy(&data[ 8], data,  8);
+    memcpy(&data[16], data, 16);
+    memcpy(&data[32], data, 24);
+
+    spi::beginTransaction(_cfg.spi_host, _cfg.freq, 0);
+    lgfx::gpio_lo(_cfg.pin_cs);
+    spi::readBytes(_cfg.spi_host, data, 57);
+    spi::endTransaction(_cfg.spi_host);
+    lgfx::gpio_hi(_cfg.pin_cs);
+
+    std::size_t ix = 0, iy = 0, iz = 0;
+    std::uint_fast16_t xt[7], yt[7], zt[7];
+    for (std::size_t j = 0; j < 7; ++j)
     {
-      memset(data, 0, 8);
-      data[ 0] = 0xB1;
-      data[ 2] = 0xC1;
-      data[ 4] = 0x91;
-      data[ 6] = 0xD1;
-      memcpy(&data[ 8], data,  8);
-      memcpy(&data[16], data, 16);
-      memcpy(&data[32], data, 24);
-      data[54] = 0xD0; // last power off.
-      data[56] = 0;
-
-      spi::beginTransaction(_cfg.spi_host, _cfg.freq, 0);
-      lgfx::gpio_lo(_cfg.pin_cs);
-      spi::readBytes(_cfg.spi_host, data, 57);
-      spi::endTransaction(_cfg.spi_host);
-      lgfx::gpio_hi(_cfg.pin_cs);
-
-      for (std::size_t j = 0; j < 7; ++j)
+      auto d = &data[j * 8];
+      int x = (d[5] << 8 | d[6]) >> 3;
+      int y = (d[1] << 8 | d[2]) >> 3;
+      int z = 0x3200 + y - x
+            + (((d[3] << 8 | d[4])
+              - (d[7] << 8 | d[8])) >> 1);
+      if (x > 128 && x <= 3968)
       {
-        auto d = &data[j * 8];
-        int x = (d[7] << 8 | d[8]) >> 3;
-        int y = (d[5] << 8 | d[6]) >> 3;
-        int z = 0x3000 + y - x
-              + (((d[1] << 8 | d[2])
-                - (d[3] << 8 | d[4])) >> 1);
-        if (z <= 0) return 0;
-        xt[i * 7 + j] = x;
-        yt[i * 7 + j] = y;
-        zt[i * 7 + j] = z;
+        xt[ix++] = x;
+      }
+      if (y > 128 && y <= 3968)
+      {
+        yt[iy++] = y;
+      }
+      if (z > 0)
+      {
+        zt[iz++] = z;
       }
     }
+    if (ix < 3 || iy < 3 || iz < 3)
+    {
+      return 0;
+    }
 
-    std::partial_sort(xt, xt+12, xt+21);
-    tp->x = (xt[8]+xt[9]+xt[10]+xt[11]) >> 2;
+    std::sort(xt, xt + ix);
+    std::sort(yt, yt + iy);
+    std::sort(zt, zt + iz);
 
-    std::partial_sort(yt, yt+12, yt+21);
-    tp->y = (yt[8]+yt[9]+yt[10]+yt[11]) >> 2;
-
-    std::partial_sort(zt, zt+11, zt+21);
-    tp->size = zt[10] >> 8;
+    tp->x    = xt[ix>>1];
+    tp->y    = yt[iy>>1];
+    tp->size = zt[iz>>1] >> 8;
 
     return tp->size ? 1 : 0;
   }
