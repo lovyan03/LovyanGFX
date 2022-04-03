81f47938dec870bf1f789c576931d4fa9b898a2d
tobozo <tobozo@users.noreply.github.com>
2022-04-03 12:47:02 +0200

    fix arduino core version detection

--- src/lgfx/v1/platforms/esp32/common.cpp
+++ src/lgfx/v1/platforms/esp32/common.cpp
@@ -736,6 +736,7 @@ namespace lgfx
 
 #if defined ( ARDUINO ) && defined ( ESP_IDF_VERSION_VAL )
  #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)
+  #include "core_version.h"
   #if ARDUINO_ESP32_GIT_VER != 0x44c11981
       auto twowire = ((i2c_port == 1) ? &Wire1 : &Wire);
       twowire->end();
