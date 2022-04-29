f7c6c46520f5a5bc85d4f0a057ac1c4bc2b9c57d
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2022-04-29 09:26:33 +0900

    tweak example for SDL2

--- examples/Advanced/CMake_SDL/LGFXSDL.cpp
+++ examples/Advanced/CMake_SDL/LGFXSDL.cpp
@@ -17,36 +17,12 @@ static int loopThread(void*)
   }
 }
 
-static int taskThread(void*)
-{
-  for (;;)
-  {
-    lgfx::Panel_sdl::sdl_event_handler();
-    std::this_thread::yield();
-  }
-}
-
 int main(int, char**)
 {
-//*
-//    setup();
-//    SDL_CreateThread(loopThread, "loopThread", nullptr);
   std::thread sub_thread(loopThread, nullptr);
   for (;;)
   {
     std::this_thread::yield();
     lgfx::Panel_sdl::sdl_event_handler();
-//    SDL_Delay(1);
   }
-/*/
-  setup();
-//  std::thread sub_thread(taskThread);
-  SDL_CreateThread(taskThread, "taskThread", nullptr);
-  for (;;)
-  {
-    loop();
-    SDL_Delay(1);
-    std::this_thread::yield();
-  }
-//*/
 }
