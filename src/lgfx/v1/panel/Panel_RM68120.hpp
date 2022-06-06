eda6afd7e62338a0d079ba655c5d88d567fa8703
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2022-06-06 14:03:55 +0900

    add Panel_RM68120 ( unverified. ) ( #190 #244 )

---
+++ src/lgfx/v1/panel/Panel_RM68120.hpp
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------/
+  Lovyan GFX - Graphics library for embedded devices.
+
+Original Source:
+ https://github.com/lovyan03/LovyanGFX/
+
+Licence:
+ [FreeBSD](https://github.com/lovyan03/LovyanGFX/blob/master/license.txt)
+
+Author:
+ [lovyan03](https://twitter.com/lovyan03)
+
+Contributors:
+ [ciniml](https://github.com/ciniml)
+ [mongonta0716](https://github.com/mongonta0716)
+ [tobozo](https://github.com/tobozo)
+/----------------------------------------------------------------------------*/
+#pragma once
+
+#include "Panel_LCD.hpp"
+
+namespace lgfx
+{
+ inline namespace v1
+ {
+//----------------------------------------------------------------------------
+
+  struct Panel_RM68120  : public Panel_LCD
+  {
+    Panel_RM68120(void)
+    {
+      _cfg.panel_width = _cfg.memory_width = 480;
+      _cfg.panel_height = _cfg.memory_height = 864;
+
+      _cfg.dummy_read_pixel = 16;
+    }
+
+    bool init(bool use_reset) override;
+
+  protected:
+
+    void writeRegister(uint16_t cmd, uint8_t data);
+
+    const uint8_t* getInitCommands(uint8_t listno) const override {
+      static constexpr uint8_t list0[] = {
+      };
+      switch (listno) {
+      case 0: return list0;
+      default: return nullptr;
+      }
+    }
+  };
+
+//----------------------------------------------------------------------------
+ }
+}
