fb59d3fd4b87f5abda9514b43edf0df16cf7ccac
tobozo <tobozo@users.noreply.github.com>
2022-07-13 15:38:37 +0200

    Improved Cache strategy for OpenCV builds

--- .github/scripts/installOpenCV.bat
+++ .github/scripts/installOpenCV.bat
@@ -1,27 +1,27 @@
 ::@echo off
 setlocal enabledelayedexpansion
 ::DO_NOT_CHANGE::
-::============================================::
+:: ============================================::
 set cvVersion=4.5.5
 echo Installing OpenCV-%cvVersion%
-::============================================::
+:: ============================================::
 mkdir opencv-%cvVersion%
 cd opencv-%cvVersion%
 mkdir Installation
 set count=1
 set cwd=%cd%
-::============================================::
+:: ============================================::
 echo Downloading opencv from github
-::download opencv from git
-git clone https://github.com/opencv/opencv.git
+:: download latest opencv from git
+git clone https://github.com/opencv/opencv.git -b %cvVersion% --depth=1
 cd opencv
-::checkout appropriate cv version
-git checkout %cvVersion%
-cd ..
-::============================================::
-echo Compiling using cmake
-cd opencv
+:: checkout appropriate cv version
+:: git checkout %cvVersion%
+:: free some space on the disk
+rmdir -Force -Recurse .git
 
+:: ============================================::
+echo Compiling using cmake
 
 set cwd=%cd%
 
@@ -31,14 +31,13 @@ echo ============================================
 
 mkdir build
 cd build
-::xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx::
-::Configure OpenCV
-cmake -G "Visual Studio 17 2022" -T host=x64 -DCMAKE_INSTALL_PREFIX=%cwd%/Installation -D BUILD_opencv_java=OFF -D BUILD_opencv_python=OFF ..
+:: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx::
+:: Configure OpenCV
+cmake -G "Visual Studio 17 2022" -T host=x64 -DCMAKE_INSTALL_PREFIX=%cwd%/Installation -D BUILD_opencv_java=OFF -D BUILD_opencv_python=OFF -D BUILD_EXAMPLES=OFF -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF ..
 ::DO_NOT_CHANGE::
 ::============================================::
-::Compile OpenCV in release mode
+:: Compile OpenCV in release mode
 cmake.exe --build . --config Debug --target INSTALL
 cd ..
 cd ..
 cd ..
-
