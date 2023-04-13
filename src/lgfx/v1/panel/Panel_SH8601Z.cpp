69e0bf6ce345c5c04d42cbfc3657b3dcf7ea39d4
Forairaaaaa <applesyqd@outlook.com>
2023-04-13 17:31:45 +0800

    add virtual override

--- src/lgfx/v1/panel/Panel_SH8601Z.cpp
+++ src/lgfx/v1/panel/Panel_SH8601Z.cpp
@@ -27,7 +27,115 @@ namespace lgfx
  {
 //----------------------------------------------------------------------------
 
+    /* Panel init */
+    bool Panel_SH8601Z::init(bool use_reset)
+    {
+        if (!Panel_Device::init(use_reset)) {
+            return false;
+        }
 
+
+        /* Init command */
+        
+
+        return false;
+    }
+
+
+    void Panel_SH8601Z::beginTransaction(void)
+    {
+
+    }
+
+
+    void Panel_SH8601Z::endTransaction(void)
+    {
+
+    }
+
+
+    color_depth_t Panel_SH8601Z::setColorDepth(color_depth_t depth)
+    {
+
+        return lgfx::rgb565_2Byte;
+    }
+
+
+    void Panel_SH8601Z::setRotation(uint_fast8_t r)
+    {
+
+    }
+
+    void Panel_SH8601Z::setInvert(bool invert)
+    {
+
+    }
+
+    void Panel_SH8601Z::setSleep(bool flg)
+    {
+
+    }
+    
+    void Panel_SH8601Z::setPowerSave(bool flg)
+    {
+
+    }
+
+    void Panel_SH8601Z::waitDisplay(void)
+    {
+
+    }
+
+    bool Panel_SH8601Z::displayBusy(void)
+    {
+
+        return false;
+    }
+
+    void Panel_SH8601Z::writePixels(pixelcopy_t* param, uint32_t len, bool use_dma)
+    {
+
+    }
+
+    void Panel_SH8601Z::writeBlock(uint32_t rawcolor, uint32_t len)
+    {
+
+    }
+
+    void Panel_SH8601Z::setWindow(uint_fast16_t xs, uint_fast16_t ys, uint_fast16_t xe, uint_fast16_t ye)
+    {
+
+    }
+
+    void Panel_SH8601Z::drawPixelPreclipped(uint_fast16_t x, uint_fast16_t y, uint32_t rawcolor)
+    {
+
+    }
+
+    void Panel_SH8601Z::writeFillRectPreclipped(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, uint32_t rawcolor)
+    {
+
+    }
+
+    void Panel_SH8601Z::writeImage(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, pixelcopy_t* param, bool use_dma)
+    {
+
+    }
+
+    uint32_t Panel_SH8601Z::readCommand(uint_fast16_t cmd, uint_fast8_t index, uint_fast8_t len)
+    {
+        return 0;
+    }
+
+    uint32_t Panel_SH8601Z::readData(uint_fast8_t index, uint_fast8_t len)
+    {
+        return 0;
+    }
+
+    void Panel_SH8601Z::readRect(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, void* dst, pixelcopy_t* param)
+    {
+
+    }
 
 
 //----------------------------------------------------------------------------
--- src/lgfx/v1/panel/Panel_SH8601Z.hpp
+++ src/lgfx/v1/panel/Panel_SH8601Z.hpp
@@ -27,8 +27,33 @@ namespace lgfx
 
   struct Panel_SH8601Z : public Panel_Device
   {
-  public:
-    Panel_SH8601Z(void) {}
+    public:
+        Panel_SH8601Z(void) {}
+
+        bool init(bool use_reset) override;
+        void beginTransaction(void) override;
+        void endTransaction(void) override;
+
+        color_depth_t setColorDepth(color_depth_t depth) override;
+        void setRotation(uint_fast8_t r) override;
+        void setInvert(bool invert) override;
+        void setSleep(bool flg) override;
+        void setPowerSave(bool flg) override;
+
+        void waitDisplay(void) override;
+        bool displayBusy(void) override;
+
+        void writePixels(pixelcopy_t* param, uint32_t len, bool use_dma) override;
+        void writeBlock(uint32_t rawcolor, uint32_t len) override;
+
+        void setWindow(uint_fast16_t xs, uint_fast16_t ys, uint_fast16_t xe, uint_fast16_t ye) override;
+        void drawPixelPreclipped(uint_fast16_t x, uint_fast16_t y, uint32_t rawcolor) override;
+        void writeFillRectPreclipped(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, uint32_t rawcolor) override;
+        void writeImage(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, pixelcopy_t* param, bool use_dma) override;
+
+        uint32_t readCommand(uint_fast16_t cmd, uint_fast8_t index, uint_fast8_t len) override;
+        uint32_t readData(uint_fast8_t index, uint_fast8_t len) override;
+        void readRect(uint_fast16_t x, uint_fast16_t y, uint_fast16_t w, uint_fast16_t h, void* dst, pixelcopy_t* param) override;
 
 
 
