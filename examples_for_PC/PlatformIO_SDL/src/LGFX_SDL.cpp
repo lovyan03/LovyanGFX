d9b797c26c1cf363ccb7ac6700ea6a6c809a816c
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-07-09 19:47:45 +0900

    tweak for PlatformIO `platform=native`

--- examples_for_PC/PlatformIO_SDL/src/LGFX_SDL.cpp
+++ examples_for_PC/PlatformIO_SDL/src/LGFX_SDL.cpp
@@ -1,21 +1,9 @@
 
 #include <lgfx/v1/platforms/sdl/Panel_sdl.hpp>
 #if defined ( SDL_h_ )
-#include <thread>
 
 void setup(void);
 void loop(void);
-
-static void loopThread(void)
-{
-  setup();
-  SDL_Delay(5);
-  for (;;)
-  {
-    std::this_thread::yield();
-    loop();
-  }
-}
 
 #if __has_include(<windows.h>)
 #include <windows.h>
@@ -25,12 +13,11 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdL
 int main(int, char**)
 #endif
 {
-  std::thread sub_thread(loopThread);
+  setup();
   for (;;)
   {
-    std::this_thread::yield();
     lgfx::Panel_sdl::sdl_event_handler();
-    SDL_Delay(5);
+    loop();
   }
   return 1;
 }
