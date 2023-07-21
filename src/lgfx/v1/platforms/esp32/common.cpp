ff7dc4dbda75646c51fca299d1c47740d98115ca
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-07-22 00:01:28 +0900

    fix #426

--- src/lgfx/v1/platforms/esp32/common.cpp
+++ src/lgfx/v1/platforms/esp32/common.cpp
@@ -479,7 +479,9 @@ namespace lgfx
         buscfg.max_transfer_sz = 1;
         buscfg.flags = SPICOMMON_BUSFLAG_MASTER;
         buscfg.intr_flags = 0;
-
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0))
+        buscfg.isr_cpu_id = INTR_CPU_ID_AUTO;
+#endif
         if (ESP_OK != spi_bus_initialize(static_cast<spi_host_device_t>(spi_host), &buscfg, dma_channel))
         {
           ESP_LOGW("LGFX", "Failed to spi_bus_initialize. ");
