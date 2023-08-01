ecd6cf2627042b5e4dce80c07a56ce5ad1d9fa3f
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2023-08-01 21:58:58 +0900

    ESP32 Light_PWM bug fix

--- src/lgfx/v1/platforms/esp32/Light_PWM.cpp
+++ src/lgfx/v1/platforms/esp32/Light_PWM.cpp
@@ -32,12 +32,15 @@ namespace lgfx
  {
 //----------------------------------------------------------------------------
 
+  static constexpr const uint8_t PWM_BITS = 9;
+
+
   bool Light_PWM::init(uint8_t brightness)
   {
 
 #if defined ( ARDUINO )
 
-    ledcSetup(_cfg.pwm_channel, _cfg.freq, 10);
+    ledcSetup(_cfg.pwm_channel, _cfg.freq, PWM_BITS);
     ledcAttachPin(_cfg.pin_bl, _cfg.pwm_channel);
     setBrightness(brightness);
 
@@ -54,7 +57,7 @@ namespace lgfx
      ledc_channel.channel    = (ledc_channel_t)_cfg.pwm_channel;
      ledc_channel.intr_type  = LEDC_INTR_DISABLE;
      ledc_channel.timer_sel  = (ledc_timer_t)((_cfg.pwm_channel >> 1) & 3);
-     ledc_channel.duty       = _cfg.invert ? 1024 : 0;
+     ledc_channel.duty       = _cfg.invert ? (1 << PWM_BITS) : 0;
      ledc_channel.hpoint     = 0;
     };
     ledc_channel_config(&ledc_channel);
@@ -65,7 +68,7 @@ namespace lgfx
 #else
       ledc_timer.speed_mode = LEDC_LOW_SPEED_MODE;
 #endif
-      ledc_timer.duty_resolution = (ledc_timer_bit_t)10; // resolution of PWM duty
+      ledc_timer.duty_resolution = (ledc_timer_bit_t)PWM_BITS; // resolution of PWM duty
       ledc_timer.freq_hz = _cfg.freq;                        // frequency of PWM signal
       ledc_timer.timer_num = ledc_channel.timer_sel;    // timer index
     };
@@ -86,11 +89,11 @@ namespace lgfx
       uint_fast16_t ofs = _cfg.offset;
       if (ofs) { ofs = ofs * 259 >> 8; }
       duty = brightness * (257 - ofs);
-      duty += (ofs * 255) + 32;
-      duty += 32;
-      duty >>= 6;
+      duty += ofs * 255;
+      duty += 1 << (15 - PWM_BITS);
+      duty >>= 16 - PWM_BITS;
     }
-    if (_cfg.invert) duty = 1024 - duty;
+    if (_cfg.invert) duty = (1 << PWM_BITS) - duty;
 
 #if defined ( ARDUINO )
       ledcWrite(_cfg.pwm_channel, duty);
