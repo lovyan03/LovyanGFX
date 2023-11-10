53e52cf3a8dfd2d28b10da1685037c9f65c6fc99
Michael Haberler <git@mah.priv.at>
2023-11-10 09:17:14 +0100

    make the CMake_WASM example work again

--- examples_for_PC/CMake_WASM/LGFX_WASM.cpp
+++ examples_for_PC/CMake_WASM/LGFX_WASM.cpp
@@ -6,16 +6,15 @@
 void setup(void);
 void loop(void);
 
-void loopThread(void *arg)
+void loopThread(void)
 {
+  lgfx::Panel_sdl::loop();
   loop();
-  lgfx::Panel_sdl::sdl_event_handler();
 }
-
-int monitor_hor_res, monitor_ver_res;
 
 int main(int argc, char **argv)
 {
   setup();
-  emscripten_set_main_loop_arg(loopThread, NULL, -1, true);
+  lgfx::Panel_sdl::setup();
+  emscripten_set_main_loop(loopThread, -1, true);
 }
