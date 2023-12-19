3c71d88b7a6928266c5cb9437479d586fe27ad18
tobozo <tobozo@users.noreply.github.com>
2023-12-19 16:07:13 +0100

    testing dynamic matrix for esp-idf versions

--- .github/scripts/esp-idf-versions.php
+++ .github/scripts/esp-idf-versions.php
@@ -105,6 +105,7 @@ foreach( $idf_versions as $idx => $idf_version )
 // add hardcoded versions
 $idf_fqbns[] = 'esp32@4.1.4';
 $idf_fqbns[] = 'esp32@4.3.6';
+$idf_fqbns[] = 'esp32@5.2';
 
 $json_array = [ "esp-idf-fqbn" => $idf_fqbns ];
 
