f3c587d915e641f18944fa0510d9069a45fe33c6
Wu SiYu <wu.siyu@hotmail.com>
2024-01-22 01:58:34 +0800

    fix Light_PWM backlight not works under arduino-esp32 v3.0 (IDF 5)

--- src/lgfx/v1/platforms/esp32/Light_PWM.cpp
+++ src/lgfx/v1/platforms/esp32/Light_PWM.cpp
@@ -109,7 +109,11 @@ namespace lgfx
     if (_cfg.invert) duty = (1 << PWM_BITS) - duty;
 
 #if defined ( ARDUINO )
+#if defined LEDC_USE_IDF_V5
+      ledcWrite(_cfg.pin_bl, duty);
+#else
       ledcWrite(_cfg.pwm_channel, duty);
+#endif
 #elif SOC_LEDC_SUPPORT_HS_MODE
       ledc_set_duty(LEDC_HIGH_SPEED_MODE, (ledc_channel_t)_cfg.pwm_channel, duty);
       ledc_update_duty(LEDC_HIGH_SPEED_MODE, (ledc_channel_t)_cfg.pwm_channel);
