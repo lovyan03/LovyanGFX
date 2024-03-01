790ab46604832e00d9a49838cb6eb897fe629777
Mitch Bradley <wmb@firmworks.com>
2024-03-01 10:30:34 -1000

    Another try at enabling filesystem support without breaking IDF

--- src/lgfx/v1/lgfx_filesystem_support.hpp
+++ src/lgfx/v1/lgfx_filesystem_support.hpp
@@ -264,9 +264,9 @@ namespace lgfx
   #endif
  #endif
 
-#elif defined(stdin) // ESP-IDF, Harmony, stdio
-      // #elif defined (ESP_PLATFORM) || defined(__SAMD51_HARMONY__) || defined(stdin) // ESP-IDF, Harmony, stdio
+#elif defined (ESP_PLATFORM) || defined(__SAMD51_HARMONY__)
       // esp32 and samd51 platforms do not have FileWrapper
+#elif defined(stdin)
 
   #define LGFX_FUNCTION_GENERATOR(drawImg) \
     inline bool drawImg##File(const char *path, int32_t x = 0, int32_t y = 0, int32_t maxWidth = 0, int32_t maxHeight = 0, int32_t offX = 0, int32_t offY = 0, float scale_x = 1.0f, float scale_y = 0.0f, datum_t datum = datum_t::top_left) \
