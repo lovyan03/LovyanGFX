fb8d2a21bd96b1788ed4020eba52d64dd7b06889
tobozo <tobozo@users.noreply.github.com>
2024-05-19 12:34:32 +0200

    disabled h2/p4/c5, code reformat and documentation

--- .github/scripts/esp-idf-versions.php
+++ .github/scripts/esp-idf-versions.php
@@ -1,72 +1,105 @@
 <?php
 
+/*
+ * esp-idf-versions.php
+ *
+ * copyleft (c+) tobozo 2024
+ *
+ * builds an array of esp-idf-fqbn for a 2D workflow matrix [idf version ^ esp32 chip]
+ * by querying the official espressif support matrix and the repository tags, e.g.
+ *
+ * {
+ *    "esp-idf-fqbn": [
+ *        "esp32@5.2.1",
+ *        "esp32s2@5.2.1",
+ *        "esp32s3@5.2.1",
+ *        "esp32c6@5.2.1",
+ *        "esp32@5.1.4",
+ *        "esp32s2@5.1.4",
+ *        "esp32s3@5.1.4",
+ *        "esp32c6@5.1.4",
+ *        "esp32@5.0.6",
+ *        "esp32s2@5.0.6",
+ *        "esp32s3@5.0.6",
+ *        "esp32@4.4.7",
+ *        "esp32s2@4.4.7",
+ *        "esp32s3@4.4.7",
+ *        "esp32@4.4.6",
+ *        "esp32@4.3.6",
+ *        "esp32@4.1.4"
+ *    ]
+ * }
+ *
+ */
+
+
 $max_versions   = 4;
-$releases       = [];
+$idf_releases   = [];
 $patch_versions = [];
 $idf_fqbns      = [];
 $idf_versions   = []; // extracted from git ls
 $idf_official_versions = []; // extracted from their readme
-$idf_boards     = ['esp32', 'esp32s2', 'esp32s3', 'esp32c6', 'esp32h2', 'esp32p4', 'esp32c5'];
+$idf_boards     = ['esp32', 'esp32s2', 'esp32s3', 'esp32c6'/*, 'esp32h2', 'esp32p4', 'esp32c5'*/];
 
-$git_remote = `git ls-remote https://github.com/espressif/esp-idf`;
 
-$esp_idf_readme_url = 'https://raw.githubusercontent.com/espressif/esp-idf/master/README.md';
-$readme_anchor_begin = '# ESP-IDF Release and SoC Compatibility';
-$readme_anghor_end = '# Developing With ESP-IDF';
-
-$readmeContents = file_get_contents($esp_idf_readme_url) or die("Unable to fetch URL ".$esp_idf_readme_url);
+// get the official support matrix from the readme on the main espidf branch.
+// it contains tabulated chips and their supported versions in markdown format
+// this allows associating each version with a set of supported chips in the CI matrix
+$idf_readme_url = 'https://raw.githubusercontent.com/espressif/esp-idf/master/README.md';
+// fetch the readme or die
+$readmeContents = file_get_contents($idf_readme_url) or die("Unable to fetch URL ".$idf_readme_url);
+// split document into an array of lines
 $lines = explode(PHP_EOL, $readmeContents);
-
-$matrix = [];
-
 foreach($lines as $line)
 {
+  // Markdown uses "|" as a column separator, at least 5 of those are expected.
+  // It is also speculated that there is only one table in the document.
   $columns_count = substr_count($line, "|");
   if( $columns_count < 5 )
     continue;
-  $columns = explode("|", $line);
-  foreach($columns as $idx => $value)
+  // remove/clean markdown from cells contents
+  $cells = explode("|", $line);
+  foreach($cells as $idx => $value)
   {
     $value = trim($value);
     $value = str_replace(array("!", "[", "]", "alt text", "(", ")", "Announcement"), "", $value);
-    $columns[$idx] = $value;
+    $cells[$idx] = $value;
   }
-
-  if(count($matrix)==0)
+  // collect/validate cell values
+  if(count($idf_official_versions)==0)
   {
-    if($columns[1]!="Chip")
+    if($cells[1]!="Chip")
     {
-      print_r($columns);
-      die("can't find the support matrix! expects 'Chip' at index 1 but got ".$columns[1]);
+      print_r($cells);
+      die("can't find the support matrix! expects 'Chip' at index 1 but got ".$cells[1]);
     }
-    foreach($columns as $idx => $cell)
+    foreach($cells as $idx => $cell)
     {
       if( preg_match("/^v[0-9.]+$/", $cell) )
       {
         $idf_official_versions[$idx] = [ "version" => $cell, "chips" => [] ];
       }
     }
-  } else
+  }
+  else
   {
     if( count($idf_official_versions) == 0)
       die("failed to collect official versions");
-    if(!preg_match("/^ESP32/", $columns[1]))
-      continue; // separator
-    $idf_board_name = strtolower(str_replace("-", "", $columns[1]));
-    for($i=2;$i<count($columns)-1;$i++)
+    if(!preg_match("/^ESP32/", $cells[1]))
+      continue; // separator or end of tabular data
+    $idf_board_name = strtolower(str_replace("-", "", $cells[1])); // e.g. ESP32-C3 -> esp32c3
+    for($i=2;$i<count($cells)-1;$i++)
     {
-      if($columns[$i]=='supported')
+      if($cells[$i]=='supported')
         $idf_official_versions[$i]['chips'][] = $idf_board_name;
     }
   }
-  $matrix[count($matrix)] = $columns;
 }
 
-//print_r($idf_official_versions);
-
+// matches $idf_version **minor/patch* version with its officially supported *major* version
+// for a given $idf_board esp32 chip
 function board_supported_by( $idf_board, $idf_version )
 {
-  //echo sprintf("%s, %s".PHP_EOL, $idf_board, $idf_version);
   global $idf_official_versions;
   foreach( $idf_official_versions as $idx => $chipsPerVersion )
   {
@@ -82,6 +115,11 @@ function board_supported_by( $idf_board, $idf_version )
 }
 
 
+// get the idf version numbers associated with their release state.
+// this allows tag based filtering if beta/rc/unstable releases while
+// resolving to the latest stable patch version (e.g. 5.2.x => 5.2.1)
+$git_remote = `git ls-remote https://github.com/espressif/esp-idf`;
+
 !empty($git_remote) or php_die("bad github response");
 
 $lines = explode("\n", $git_remote);
@@ -96,12 +134,12 @@ foreach( $lines as $num => $line )
     continue; // EOL or separator
     $line_parts = explode("/", trim($line)); // tag name is the last part
     if( !empty( $line_parts ) )
-      $releases[] = end($line_parts);
+      $idf_releases[] = end($line_parts);
 }
 
-!empty($releases) or php_die("releases not found");
+!empty($idf_releases) or php_die("releases not found");
 
-arsort( $releases );
+arsort( $idf_releases );
 
 // get version numbers from enumerated tags
 foreach( $lines as $num => $line )
@@ -126,7 +164,7 @@ foreach( $lines as $num => $line )
       //php_die("uh oh pattern matching failed with $semver/$tag_name");
       $minor = $results[1].'.'.$results[2];
     $patch = !empty($results[3]) ? $results[1].'.'.$results[2].'.'.$results[3] : $minor;
-    if( !in_array( 'v'.$minor, $releases ) )
+    if( !in_array( 'v'.$minor, $idf_releases ) )
       continue; // this tag is not listed in releases
       if( /*!empty($results[3]) &&*/ !in_array( $patch, $patch_versions ) )
         $patch_versions[] = $patch;
@@ -140,7 +178,7 @@ arsort( $patch_versions );
 $max_boards = (count($idf_boards)*$max_versions);
 
 // match release versions with tag versions
-foreach( $releases as $minor )
+foreach( $idf_releases as $minor )
 {
   $top_version = '';
   foreach( $patch_versions as $patch )
