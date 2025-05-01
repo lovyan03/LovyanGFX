589f014f320e15628bff4c4bf92d0a7fd1213f8d
tobozo <tobozo@users.noreply.github.com>
2025-05-01 11:51:59 +0200

    added QSPI support to lgfx::Bus_SPI, deleted Bus_QSPI

--- src/lgfx/v1/platforms/device.hpp
+++ src/lgfx/v1/platforms/device.hpp
@@ -38,7 +38,6 @@ Contributors:
 
   #include "esp32/Light_PWM.hpp"
   #include "esp32/Bus_SPI.hpp"
-  #include "esp32/Bus_QSPI.hpp"
   #include "esp32/Bus_I2C.hpp"
   #include "esp32s2/Bus_Parallel8.hpp"
   #include "esp32s2/Bus_Parallel16.hpp"
@@ -47,7 +46,6 @@ Contributors:
 
   #include "esp32/Light_PWM.hpp"
   #include "esp32/Bus_SPI.hpp"
-  #include "esp32/Bus_QSPI.hpp"
   #include "esp32/Bus_I2C.hpp"
   #include "esp32s3/Bus_Parallel8.hpp"
   #include "esp32s3/Bus_Parallel16.hpp"
